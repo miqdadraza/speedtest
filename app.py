525cec4381b358de582526862167d54e09c17cca
Miqdad Raza <miqdad@bhurani.net>
2021-05-28 18:39:47 -0700

    Wrote pseudocode and program description

---
+++ app.py
@@ -0,0 +1,21 @@
+"""
+    This python application will test your internet speed, and email and text it to you.
+    In addition, it will also store the date/time, and the upload speed, download speed, and ping to a file.
+    From this file, it will generate an interactive graph, and save this graph as html.
+    You will also get this graph in your email.
+
+"""
+
+"""
+    Psuedocode:
+    1) Speed:
+        a. Get ping and store it in a variable.
+        b. Get upload speed and store it in a variable.
+        c. Get download speed and store it in a variable.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main() 
